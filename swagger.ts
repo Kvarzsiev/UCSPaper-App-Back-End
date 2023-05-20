5712677493f12d5b8367bd6f22abec67a01a2d49
Marcellofabrizio <marcfabrizio10@gmail.com>
2023-05-20 18:41:17 -0300

    Adds swagger

---
+++ swagger.ts
@@ -0,0 +1,18 @@
+export const swaggerDocument = {
+  openapi: '3.0.1',
+  info: {
+    version: '1.0.0',
+    title: 'Lattes App API',
+    description: 'Documentação da API do projeto Lattes',
+    termsOfService: '',
+    contact: {
+      name: 'Marcello',
+    },
+    license: {
+      name: 'Apache 2.0',
+      url: 'https://www.apache.org/licenses/LICENSE-2.0.html',
+    },
+  },
+  paths: {},
+  definitions: {},
+};
