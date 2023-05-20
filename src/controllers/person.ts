03fa5d8db49ef1e9ea21b8759c3f8843e602971d
Marcellofabrizio <marcfabrizio10@gmail.com>
2023-05-20 15:57:51 -0300

    Adds person get and get by id

---
+++ src/controllers/person.ts
@@ -0,0 +1,45 @@
+import { Request, Response, NextFunction } from 'express';
+import { getRepository } from 'typeorm';
+
+import { Person } from 'entities/Person/Person';
+import { CustomError } from 'utils/customError';
+
+export async function getPersons(req: Request, res: Response, next: NextFunction) {
+  const personRepository = await getRepository(Person);
+
+  try {
+    const persons = await personRepository.find({
+      select: ['id', 'name', 'email', 'institution'],
+    });
+    res.status(200).send(persons);
+  } catch (err) {
+    const customError = new CustomError(400, 'Raw', 'Cant retrieve list of persons', null, err);
+    return next(customError);
+  }
+}
+
+export async function getPersonById(req: Request, res: Response, next: NextFunction) {
+  const id = Number(req.params.id);
+
+  const personRepository = await getRepository(Person);
+
+  try {
+    const person = await personRepository.findOne({
+      where: {
+        id: id,
+      },
+      select: ['id', 'name', 'email', 'institution'],
+      relations: ['personProjects'],
+    });
+
+    if (!person) {
+      const customError = new CustomError(404, 'Not Found', 'User not found', ['User not found']);
+      return next(customError);
+    }
+
+    res.status(200).send(person);
+  } catch (err) {
+    const customError = new CustomError(400, 'Raw', `Can't retrieve list of users.`, null, err);
+    return next(customError);
+  }
+}
