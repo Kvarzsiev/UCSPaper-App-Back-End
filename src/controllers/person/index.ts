908f75d599b95a489ae207b06022fd4babbb72b9
Marcellofabrizio <marcfabrizio10@gmail.com>
2023-05-20 19:03:03 -0300

    Post person controller

--- src/controllers/person/index.ts
+++ src/controllers/person/index.ts
@@ -33,13 +33,32 @@ export async function getPersonById(req: Request, res: Response, next: NextFunct
     });
 
     if (!person) {
-      const customError = new CustomError(404, 'Not Found', 'User not found', ['User not found']);
+      const customError = new CustomError(404, 'Not Found', 'User not found');
       return next(customError);
     }
 
     res.status(200).send(person);
   } catch (err) {
     const customError = new CustomError(400, 'Raw', `Can't retrieve list of users.`, null, err);
+    return next(customError);
+  }
+}
+
+export async function createPerson(req: Request, res: Response, next: NextFunction) {
+  const { name, email, institution } = req.body;
+
+  const personRepository = await AppDataSource.getRepository(Person);
+
+  try {
+    const person = new Person();
+    person.name = name;
+    person.email = email;
+    person.institution = institution;
+
+    await personRepository.save(person);
+    res.status(200).send(person);
+  } catch (err) {
+    const customError = new CustomError(400, 'Raw', `Could not create person`, null, err);
     return next(customError);
   }
 }
