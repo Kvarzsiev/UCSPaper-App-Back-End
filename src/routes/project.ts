76dc7fda6e4f53c68a9b8e2d521f195143aba08f
Marcellofabrizio <marcfabrizio10@gmail.com>
2023-05-30 00:12:26 -0300

    Adds not implemented error for remove project and person endpoints

--- src/routes/project.ts
+++ src/routes/project.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { getProjects, getProjectById, createProject, editProject } from 'controllers/project';
+import { CustomError } from 'utils/customError';
 
 const router = Router();
 
@@ -7,5 +8,11 @@ router.get('/projects', getProjects);
 router.get('/projects/:id', getProjectById);
 router.post('/projects', createProject);
 router.put('/projects/:id', editProject);
+router.put('/projects/:id/removePerson', (req, res, next) => {
+  return next(new CustomError(501, 'Raw', 'Remove Person from project not supported'));
+});
+router.put('/projects/:id/removeResult', (req, res, next) => {
+  return next(new CustomError(501, 'Raw', 'Remove Result from project not supported'));
+});
 
 export default router;
