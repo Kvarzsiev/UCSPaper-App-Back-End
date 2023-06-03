73688851be412e9c67de9d0174a077e8f1e6e854
Marcellofabrizio <marcfabrizio10@gmail.com>
2023-06-02 22:08:48 -0300

    PersonProject service

---
+++ src/services/personProject/index.ts
@@ -0,0 +1,20 @@
+import { PersonProject } from 'entities/PersonProject/PersonProject';
+import { AppDataSource } from 'database/dataSource';
+import { Repository } from 'typeorm';
+
+export async function fetchPersonProject(personId: number, projectId: number): Promise<PersonProject> {
+  const personProjectRepository = await AppDataSource.getRepository(PersonProject);
+  return personProjectRepository
+    .createQueryBuilder('personProject')
+    .select('personProject')
+    .where('personProject.person_id = :personId AND personProject.project_id = :projectId', {
+      projectId: projectId,
+      personId: personId,
+    })
+    .getOne();
+}
+
+export async function savePersonProject(personProject: PersonProject): Promise<PersonProject> {
+  const personProjectRepository: Repository<PersonProject> = await AppDataSource.getRepository(PersonProject);
+  return personProjectRepository.save(personProject);
+}
